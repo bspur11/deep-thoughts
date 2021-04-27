0f454f5ebf32ca97c17e0987324b0223d5d8db2e
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-04-27 16:10:19 -0400

    one down

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,26 @@ const resolvers = {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
+
+    // place this inside of the `Query` nested object right after `though
+thought: async (parent, { _id }) => {
+  return Thought.findOne({ _id });
+},
+
+// get all users
+users: async () => {
+  return User.find()
+    .select('-__v -password')
+    .populate('friends')
+    .populate('thoughts');
+},
+// get a user by username
+user: async (parent, { username }) => {
+  return User.findOne({ username })
+    .select('-__v -password')
+    .populate('friends')
+    .populate('thoughts');
+},
   }
 };
 
